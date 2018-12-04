92d0f1fd5d400b123e5294dcfb4ef2243e937fbb
383523223@qq.com <383523223@qq.com>
2018-12-04 09:47:19 +0800

    注释无用代码

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.locals.app = require('config').app;
+// app.locals.app = require('config').app;
 
 app.disable('x-powered-by');
 
@@ -32,8 +32,8 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
